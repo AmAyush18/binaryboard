52cdd086ba850d0d122527b23038ed1bf0ba14fa
Ayush Dwivedi <amayush0818@gmail.com>
2025-02-13 18:42:50 +0530

    Implemented webhook to save user data to convex

---
+++ frontend/convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://curious-whippet-47.clerk.accounts.dev",
+      applicationID: "binaryboard",
+    },
+  ],
+};
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,15 @@
+import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <SignedOut>
-        <SignInButton />
+        <SignInButton>
+          <Button>
+            Login using Clerk
+          </Button>
+        </SignInButton>
       </SignedOut>
 
       <SignedIn>
