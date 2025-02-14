c65afb54b2fd9430fc0227fd04cb5209e59ff479
Ayush Dwivedi <amayush0818@gmail.com>
2025-02-15 03:53:56 +0530

    Completed VideoCall Section in MeetingRoom Page

--- frontend/src/components/MeetingRoom.tsx
+++ frontend/src/components/MeetingRoom.tsx
@@ -1,8 +1,93 @@
-import React from 'react'
+import {
+    CallControls,
+    CallingState,
+    CallParticipantsList,
+    PaginatedGridLayout,
+    SpeakerLayout,
+    useCallStateHooks,
+  } from "@stream-io/video-react-sdk";
+  import { LayoutListIcon, LoaderIcon, UsersIcon } from "lucide-react";
+  import { useRouter } from "next/navigation";
+  import { useState } from "react";
+  import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "./ui/resizable";
+  import {
+    DropdownMenu,
+    DropdownMenuTrigger,
+  } from "./ui/dropdown-menu";
+import { Button } from "./ui/button";
+
 
 const MeetingRoom = () => {
+    const router = useRouter();
+    const [layout, setLayout] = useState<"grid" | "speaker">("speaker");
+    const [showParticipants, setShowParticipants] = useState<boolean>(false);
+    const { useCallCallingState } = useCallStateHooks();
+
+    const callingState = useCallCallingState();
+
+    if (callingState !== CallingState.JOINED) {
+        return (
+            <div className="h-96 flex items-center justify-center">
+                <LoaderIcon className="size-6 animate-spin" />
+            </div>
+        )
+    }
+
   return (
-    <div>MeetingRoom</div>
+    <div className="h-[calc(100vh-4rem-1px)]">
+        <ResizablePanelGroup direction="horizontal">
+            {/* LEFT SIDE - VIDEO CALL  */}
+            <ResizablePanel defaultSize={35} minSize={25} maxSize={100} className="relative">
+                {/* VIDEO LAYOUT  */}
+                <div className="absolute inset-0">
+                    {layout === "grid" ? <PaginatedGridLayout /> : <SpeakerLayout />}
+
+                    {/* PARTICIPANTS LIST OVERLAY  */}
+                    {showParticipants && (
+                        <div className="absolute right-0 top-0 h-full w-[300px] bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+                            <CallParticipantsList onClose={() => setShowParticipants(false)} />
+                        </div>
+                    )}
+                </div>
+
+                {/* VIDEO CONTROLS  */}
+
+                <div className="absolute bottom-4 left-0 right-0">
+                    <div className="flex flex-col items-center gap-4">
+                        <div className="flex items-center gap-2 flex-wrap justify-center px-4">
+                            <CallControls onLeave={() => router.push("/")} />
+
+                            <div className="flex items-center gap-2">
+                                <DropdownMenu>
+                                    <DropdownMenuTrigger>
+                                        <Button variant="outline" size="icon" className="size-10">
+                                            <LayoutListIcon className="size-4" />
+                                        </Button>
+                                    </DropdownMenuTrigger>
+                                </DropdownMenu>
+
+                                <Button
+                                    variant="outline"
+                                    size="icon"
+                                    className="size-10"
+                                    onClick={() => setShowParticipants(!showParticipants)}
+                                >
+                                    <UsersIcon className="size-4" />
+                                </Button>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </ResizablePanel>
+            
+            <ResizableHandle withHandle />
+            {/* RIGHT SIDE - CODE EDITOR  */}
+            <ResizablePanel>
+                <h2>Code Editor Here</h2>
+            </ResizablePanel>
+        </ResizablePanelGroup>
+    </div>
   )
 }
 
